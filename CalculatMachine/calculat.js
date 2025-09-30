efe60c596f0e1dbc1e5011b15ecc7b4b8f22462d
lannistar1 <51417762+lannistar1@users.noreply.github.com>
2025-09-30 05:39:40 -0700

    Add files via upload

---
+++ CalculatMachine/calculat.js
@@ -0,0 +1,44 @@
+function calcNumbers(result) {
+    document.querySelector('.area').value += result;
+}
+function percentage(result) {
+    let result1 = String(result);
+    if(result1.includes("%")) {
+        let a, b, percent;
+        let splitWord = [];
+        a = Number(splitWord[0]);
+        b = Number(splitWord[1]);
+        percent = (a / 100 * b);
+        document.querySelector('.area').value = percent;
+    }
+}
+
+function makeNegative(result) {
+    if (result.charAt(0) == '_') {
+        document.querySelector('.area').value = result.replace('-', '');
+    }
+    else{
+        document.querySelector('.area').value = '-' + result;
+    }
+}
+
+const body = document.querySelector('body'),
+modeToggle = document.querySelector('.dark-light');
+
+modeToggle.addEventListener("click", () => {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
+    if(!body.classList.contains("dark"))
+    {
+        localStorage.setItem("mode", "light-mode");
+    }
+    else
+    {
+        localStorage.setItem("mode", "dark-mode");
+    }
+});
+
+
+
+
+
